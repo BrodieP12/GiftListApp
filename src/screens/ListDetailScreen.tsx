c5312c72ae8460d040b063a3f9f38dc9b805860d
Brodie Pasker <brodiepasker20@gmail.com>
2026-03-11 11:04:46 -0500

    Fixed bugs

--- src/screens/ListDetailScreen.tsx
+++ src/screens/ListDetailScreen.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  FlatList,
-  ActivityIndicator,
-  Alert
 // screens/ListDetailScreen.tsx
 import React, { useCallback } from 'react';
 import {
@@ -66,9 +57,6 @@ export const ListDetailScreen = ({ route, navigation }: Props) => {
           <GiftItemRow
             item={item}
             currentUserId={user?.uid ?? ''}
-          <GiftItemRow
-            item={item}
-            currentUserId={user?.uid ?? ''}
             isOwner={isOwner}
             onToggleClaim={handleToggleClaim}
           />
@@ -81,7 +69,6 @@ export const ListDetailScreen = ({ route, navigation }: Props) => {
       />
 
       {isOwner && (
-        <TouchableOpacity
         <TouchableOpacity
           style={styles.fab}
           onPress={() => navigation.navigate('AddItem', { listId: listId })}
@@ -117,18 +104,11 @@ const styles = StyleSheet.create({
   fab: {
     position: 'absolute',
     bottom: 30,
-    position: 'absolute',
-    bottom: 30,
     right: 30,
     backgroundColor: '#007AFF',
     width: 120,
     height: 50,
-    backgroundColor: '#007AFF',
-    width: 120,
-    height: 50,
     borderRadius: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
     justifyContent: 'center',
     alignItems: 'center',
     elevation: 5,
@@ -137,10 +117,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
   },
-  fabText: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 16
   fabText: {
     color: '#fff',
     fontWeight: 'bold',
